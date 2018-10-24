91f23bc946ecdf1ae57d2a4f7f25254517fcdbb2
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-10-24 10:35:06 +0200

    Solucionado acumulacion de numeros

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const container = document.querySelector(".calc");
 container.addEventListener("click", onClick);
 
-function onClick(evento) {
-  const botones = evento.target.innerText;
-  console.log(`clickado botón ${botones}`);
+const result = document.querySelector(".results");
 
-  const result = document.querySelector(".results");
-  result.innerText = botones;
+let primero = "";
+let segundo = "";
+let operador = "";
+
+function onClick(evento) {
+  const clickear = evento.target.innerText;
+  console.log(`clickado botón ${clickear}`);
+
+  result.innerText = clickear;
+
+  switch (clickear) {
+    case "C":
+      result.innerText = 0;
+      break;
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+      mantenerNum(clickear);
+
+      break;
+  }
+}
+
+function mantenerNum(value) {
+  primero += value;
+
+  result.innerText = primero;
 }
